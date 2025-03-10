558f529e65edd60d71c16a7663b273318a8fea4e
sergil <>
2025-03-10 11:56:09 -0500

    add database connection

---
+++ src/config/dbConnection.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const developer = Number(process.env.DEVELOPER);
+import { config } from "dotenv";
+
+config({ path: "./.env" });
+const mongo_key = developer ? "mongodb://localhost:27017/" : process.env.MONGO_CONNECTION;
+
+
+export default async function connection(){
+  await mongoose.connect(<any>mongo_key, {
+    dbName: "pop",
+  });
+
+  console.log("connection stablished");
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { config } from "dotenv";
+import connection from "./config/dbConnection.js";
 
 config({ path: "./.env" });
 const PORT = process.env.PORT || 3000;
 const app = express();
+connection();
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
